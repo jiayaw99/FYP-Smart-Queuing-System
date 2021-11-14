72b99a03315b5f48b4f4e9ac7ab0897312f683b1
jiayaw1999@gmail.com <jiayaw1999@gmail.com>
2021-11-14 13:14:13 +0000

    Edited settings, Form1.RowTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,3 +9,11 @@ class Form1(Form1Template):
 
     # Any code you write here will run when the form opens.
     
+
+  def data_grid_1_show(self, **event_args):
+    """This method is called when the data grid is shown on the screen"""
+    iris_category = anvil.server.call('simulation')
+
+    pass
+
+
