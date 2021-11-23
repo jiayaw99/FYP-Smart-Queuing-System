d292bae0d052a93476b5d22d7dd113d8a01bf6e8
jiayaw1999@gmail.com <jiayaw1999@gmail.com>
2021-11-23 11:29:39 +0000

    Edited ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -4,14 +4,18 @@ from anvil.tables import app_tables
 import anvil.server
 
 @anvil.server.callable
-@anvil.tables.in_transaction
-def do_update():
-  row = app_tables.queue_table.search()
+@tables.in_transaction
+def delay_Patient(doctor_number):
+  row = app_tables.queue_table.search(tables.order_by("Priority index",ascending=False))
+  skip = 0
   for data in row:
-    temp = data['Predicted waiting time']
-    if temp!="No-show":
-      temp = int(temp.split(' ')[0])+40
-      my_dict = {"Predicted waiting time": str(temp) + " minutes"}
-      app_tables.queue_table.get(Patient=data['Patient']).update(**my_dict)
+    if skip >= 1:
+      temp = data['Predicted waiting time']
+      if temp!="No-show":
+        temp = int(temp.split(' ')[0])+ int(50/doctor_number)
+        my_dict = {"Predicted waiting time": str(temp) + " minutes"}
+        app_tables.queue_table.get(Patient=data['Patient']).update(**my_dict)
+    else:
+      skip+=1
 
   
